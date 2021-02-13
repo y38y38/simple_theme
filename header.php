f38ac69410686be7bfe506be086f082d14a2c77b
y38y38 <y38y38@gmail.com>
2021-02-14 02:26:53 +0900

    debug

--- header.php
+++ header.php
@@ -2,6 +2,7 @@
 <html>
 	<head>
 		<?php wp_head(); ?>
+		<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/destyle.css" type="text/css" />
 		<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" />
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
 	</head>
--- index.php
+++ index.php
@@ -1,33 +1,46 @@
 <?php get_header(); ?>
 
-<span class="blog_title">
-<?php
-	bloginfo( 'name' );
-?>
-</span>
 
+<div class="container">
+	<div class="title_item">
+	<span class="blog_title">
+		<?php
+		bloginfo( 'name' );
+		?>
+		</span>
+		&nbsp;
+		&nbsp;
 
-&nbsp;
-&nbsp;
-
-<?php
-	bloginfo( 'description' );
-?></p>
+		<?php
+		bloginfo( 'description' );
+		?></p>
+		</br>
+	</div>
+	<div class="menu_item">
+	<div class="menu_title">
+	<?php 
+		wp_nav_menu( array( 
+			'theme_location' => 'main-menu', 
+		)); 
+		?>
+	</div>
+	</div>
 	
-<?php 
-	wp_nav_menu( array( 
-		'theme_location' => 'main-menu' 
-	)); 
-?>
+	<div class="main_item">
 
-<?php 
-if(have_posts()): 
-	while(have_posts()): 
-		the_post(); 
-    	the_post_thumbnail('thumbnail');
-	endwhile;
-endif;
-?>
+	<?php 
+		if(have_posts()): 
+			while(have_posts()): 
+				the_post(); 
+    			the_post_thumbnail('thumbnail');
+			endwhile;
+		endif;
+	?>
+	</br>
+	あいうえお</br>
+	abcdef</br>
+	</div>
 
+</div>
 <?php get_footer(); ?>
 
