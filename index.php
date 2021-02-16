65f719897f4a4cc181b656b82871b9d409f99f26
y38y38 <y38y38@gmail.com>
2021-02-16 23:07:54 +0900

    debug

--- index.php
+++ index.php
@@ -31,42 +31,44 @@
 	</div>
 	
 	<div class="main_area">
-		<div class="main_content_area" >
-			工事中
-			<?php 
-				if(have_posts()): 
-					while(have_posts()):
-			?>
-					<div class="post_produce"> 
-						<?php
-							the_post();
-							the_date('Y/m/d', '<div class="date_item">', '</div>');
-						?>
-							<h2><a href="<?php the_permalink() ?>"> <?php the_title() ?></a></h2>
-							<div class="post_produce_category">
+		<div class="main_area2"">
+			<div class="main_content_area" >
+				工事中
+				<?php 
+					if(have_posts()): 
+						while(have_posts()):
+				?>
+						<div class="post_produce"> 
 							<?php
-								$category=get_the_category();
-								foreach($category as $cate) {
-									echo '<a href="' . get_category_link( $cate->term_id ) . '">' . $cate->cat_name . '</a>';
-									echo '&nbsp;&nbsp;&nbsp;';
-								
-								}
+								the_post();
+								the_date('Y/m/d', '<div class="date_item">', '</div>');
 							?>
-							</div>
-						</br>
-						<?php
-							the_post_thumbnail('large');
-						?>
-					</div>
-			<?php
-					endwhile;
-				endif;
-			?>
-		</div>
-		<div class="sidebar_area">
-			<?php
-				get_sidebar();
-			?>
+								<h2><a href="<?php the_permalink() ?>"> <?php the_title() ?></a></h2>
+								<div class="post_produce_category">
+								<?php
+									$category=get_the_category();
+									foreach($category as $cate) {
+										echo '<a href="' . get_category_link( $cate->term_id ) . '">' . $cate->cat_name . '</a>';
+										echo '&nbsp;&nbsp;&nbsp;';
+									
+									}
+								?>
+								</div>
+							</br>
+							<?php
+								the_post_thumbnail('large');
+							?>
+						</div>
+				<?php
+						endwhile;
+					endif;
+				?>
+			</div>
+			<div class="sidebar_area">
+				<?php
+					get_sidebar();
+				?>
+			</div>
 		</div>
 	</div>
 
