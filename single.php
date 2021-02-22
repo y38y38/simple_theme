70a702ac6cd010d691137d93bfd9b77c44391016
y38y38 <y38y38@gmail.com>
2021-02-22 20:26:40 +0900

    debug

--- single.php
+++ single.php
@@ -61,6 +61,10 @@
 									the_post_thumbnail($width_and_height);
 								?>
 							</div>
+							<div class="post_content_text">
+								<?php the_content(); ?>
+							</div>
+
 						</div>
 				<?php
 						endwhile;
@@ -76,18 +80,11 @@
 	</div>
 
 </div>
-<?php get_footer(); ?>
-
-
-
-<?php get_header(); ?>
-<?php if (have_posts()):
-		while (have_posts()):
-			the_post();
-			the_content();
-		endwhile;
-	endif; ?>
 <?php previous_post_link(); ?>
 <?php next_post_link(); ?>
-
 <?php get_footer(); ?>
+
+
+
+
+
